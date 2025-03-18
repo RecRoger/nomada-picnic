b015ca72034efac4e3330ecfa531c15e88361a68
RecRoger <recroger@gmail.com>
2025-03-18 14:42:51 -0300

    start

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const port = process.env.PORT || 3000;
+
+  app.useStaticAssets(join(__dirname, '../..', 'client/dist/browser'));
+  app.setBaseViewsDir(join(__dirname, '../..', 'client/dist/browser'));
+  app.setViewEngine('html');
+
+  await app.listen(port);
+  console.log(`Application listening on port ${port}`);
+
 }
 bootstrap();

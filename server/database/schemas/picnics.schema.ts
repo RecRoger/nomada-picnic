04ad83b95f7e6a2cc5a824d25ba60b6ce5849137
RecRoger <recroger@gmail.com>
2025-03-26 01:37:06 -0300

    complete basic picnic model

--- server/database/schemas/picnics.schema.ts
+++ server/database/schemas/picnics.schema.ts
@@ -1,14 +1,66 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
 @Schema()
 export class Picnic extends Document {
-  @Prop()
-  nombre: string;
+  @Prop({ type: Object })
+  client: {
+    name: string;
+    email: string;
+    phone: string;
+  };
 
-  @Prop()
-  descripcion: string;
+  @Prop({ type: Object })
+  event: {
+    description: string;
+    guestsAmount: number;
+    date: Date;
+    extraHours: number;
+    place: mongoose.Types.ObjectId
+    newPlace: string;
+    boardText: string;
+  };
 
+  @Prop({ type: Object })
+  basicProduction: {
+    promoIndicator: boolean;
+    blanketsAmount: number;
+    tableAmount: number;
+    bigTableIndicator: boolean;
+    archIndicator: boolean;
+    gifts: [{
+      item: mongoose.Types.ObjectId,
+      amount: number
+    }]
+    basicCost: {
+      transportationCost: number;
+      giftCost: number;
+    };
+    basicEarnPercentage: number;
+    basicPrice: number;
+  };
+
+  @Prop({ type: Object })
+  additionals: {
+    items: [{
+      item: mongoose.Types.ObjectId,
+      amount: number
+    }]
+    additionalCost: number;
+    additionalEarnPercentage: number;
+    additionalPrice: number;
+  };
+
+  @Prop({ type: Object })
+  food: {
+    foods: [{
+      item: mongoose.Types.ObjectId,
+      amount: number
+    }];
+    foodCost: number;
+    foodEarnPercentage: number;
+    foodPrice: number;
+  };
 }
 
 export const PicnicsSchema = SchemaFactory.createForClass(Picnic);
--- server/modules/picnics/picnics.controller.ts
+++ server/modules/picnics/picnics.controller.ts
@@ -1,15 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { PicnicsService } from 'server/modules/picnics/picnics.service';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { ResponseDto } from 'server/models/responses.dto';
+import { Picnic } from 'server/database/schemas/picnics.schema';
+import { PicnicDto } from 'server/models/picnic.dto';
+
 @Controller({ version: '1' })
 @ApiTags('Picnics')
 export class PicnicsController {
   constructor(private readonly picnicsService: PicnicsService) { }
 
   @Get()
-  @ApiOperation({ summary: 'Consulta algun picnic' })
-  getPicnic(): string {
-    return this.picnicsService.getPicnic();
+  @ApiOperation({ summary: 'Obtiene la informacion de un picnic, consultan por id e email o apellido' })
+  @ApiResponse({ status: 201, description: 'Informacion de un picnic', type: ResponseDto<Picnic> })
+  async findByType(@Query('id') id?: string, @Query('email') email?: string, @Query('lastname') lastname?: string): Promise<Picnic> {
+    return this.picnicsService.findOne(id, email, lastname);
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Crear un nuevo costo' })
+  @ApiBody({ type: PicnicDto })
+  @ApiResponse({ status: 201, description: 'Nuevo costo creado', type: ResponseDto<Picnic> })
+  async create(@Body() picnicDto: PicnicDto): Promise<Picnic> {
+    return this.picnicsService.create(picnicDto);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Actualizar un costo existente' })
+  @ApiBody({ type: PicnicDto })
+  @ApiResponse({ status: 201, description: 'Costo editado', type: ResponseDto<Picnic> })
+  async update(@Param('id') id: string, @Body() updatePlaceDto: PicnicDto): Promise<Picnic> {
+    return this.picnicsService.update(id, updatePlaceDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un costo existente' })
+  @ApiResponse({ status: 201, description: 'Status de eliminacion', type: ResponseDto<boolean> })
+  async remove(@Param('id') id: string): Promise<boolean> {
+    return this.picnicsService.remove(id);
   }
 
 }

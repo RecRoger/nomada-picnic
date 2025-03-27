7cb5a40cf6e30c4bfa30b8924767ac1784e6179a
RecRoger <recroger@gmail.com>
2025-03-26 23:09:46 -0300

    start client scafolding

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './sections/admin/admin.component';
+import { PublicComponent } from './sections/public/public.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: '', component: PublicComponent },
+  { path: '*', redirectTo: '/public', pathMatch: 'full' }, // Ruta raíz redirige al módulo público
+];

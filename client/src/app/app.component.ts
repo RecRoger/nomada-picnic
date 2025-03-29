b45df4a049c945c84526c037c41c788edc3750cd
RecRoger <recroger@gmail.com>
2025-03-29 00:12:20 -0300

    configuracion de literales

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
-
+import { TranslateService, TranslateStore } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  providers: [AuthService]
+  providers: [AuthService, TranslateStore]
 })
 export class AppComponent {
   title = 'Cliente'
 
-  constructor() {
+  constructor(private translate: TranslateService) {
     console.log('it strart')
+    this.translate.addLangs(['en', 'es']);
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
   }
 }
--- client/src/app/app.config.server.ts
+++ client/src/app/app.config.server.ts
@@ -1,10 +1,14 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { TranslateModule } from '@ngx-translate/core';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    ...TranslateModule.forRoot({
+      defaultLanguage: 'es', // Idioma por defecto
+    }).providers!,
   ]
 };
 

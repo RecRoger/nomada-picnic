375c1bfdb43a77132ff06ac6e85895b6f06a2057
RecRoger <recroger@gmail.com>
2026-03-18 16:03:52 -0300

    barra

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // client/src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  bucketUrl: 'https://storage.googleapis.com/nomada-storage-bucket/',
+  bucketUrl: 'https://storage.googleapis.com/nomada-storage-bucket',
   apiUrl: 'https://nomada-backend-389141432152.us-east1.run.app'
 };

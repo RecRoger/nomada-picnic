0151f980045fbe81edfc8f4fd925d4fbbeb416da
RecRoger <recroger@gmail.com>
2026-03-18 15:48:13 -0300

    use bucket to upload dynamic content

--- client/src/app/core/pipes/api-image-url.pipe.ts
+++ client/src/app/core/pipes/api-image-url.pipe.ts
@@ -8,6 +8,6 @@ import { environment } from 'src/environments/environment';
 export class ApiImageUrlPipe implements PipeTransform {
   transform(value: string): string {
     if (!value) return 'assets/placeholder.png'; // Imagen por defecto
-    return `${environment.apiUrl}${value}`;
+    return `${environment.bucketUrl}/${value}`;
   }
 }
--- client/src/environments/environment.stage.ts
+++ client/src/environments/environment.stage.ts
@@ -1,5 +1,6 @@
 // client/src/environments/environment.ts
 export const environment = {
   production: false,
+  bucketUrl: 'https://storage.googleapis.com/nomada-storage-bucket/',
   apiUrl: 'https://nomada-backend-389141432152.us-east1.run.app'
 };

738add5c65109b93a528f4bf1f8ef11c9f44438d
RecRoger <recroger@gmail.com>
2026-07-05 00:51:22 -0300

    top scroll

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { TRANSLATE_HTTP_LOADER_CONFIG, TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -8,9 +8,9 @@ import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimationsAsync(),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideHttpClient(withFetch()),
     {
       provide: TRANSLATE_HTTP_LOADER_CONFIG,

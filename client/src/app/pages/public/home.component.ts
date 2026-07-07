b3626d2265745badf0f715568467a7c0cac9a4b4
RecRoger <recroger@gmail.com>
2026-07-07 15:11:14 -0300

    video in place banner, bkgrnd image in picnics banner

--- client/src/app/pages/public/home.component.ts
+++ client/src/app/pages/public/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { PlacesCarouselComponent } from '@components/places-carousel/places-carousel.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterLink } from "@angular/router";
 import { HeroComponent } from '@components/hero/hero.component';
 import { FeaturesCardsComponent } from '@components/features-cards/features-cards.component';
 import { PicnicsBannerComponent } from '@components/picnics-banner/picnics-banner.component';
 import { InfoStepsComponent } from '@components/info-steps/info-steps.component';
+import { PlacesBannerComponent } from '@components/places-banner/places-banner.component';
 
 @Component({
   selector: 'app-home',
@@ -16,12 +16,11 @@ import { InfoStepsComponent } from '@components/info-steps/info-steps.component'
     TranslateModule,
     MatIconModule,
     MatButtonModule,
-    PlacesCarouselComponent,
-    RouterLink,
     HeroComponent,
     FeaturesCardsComponent,
     PicnicsBannerComponent,
     InfoStepsComponent,
+    PlacesBannerComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'

39ffca522271d868ab37444ecdb17c8b248d6bd4
RecRoger <recroger@gmail.com>
2026-08-07 12:48:22 -0300

    seccion de picnics

--- client/src/app/pages/public/components/picnic-combos/picnic-combos.component.ts
+++ client/src/app/pages/public/components/picnic-combos/picnic-combos.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, CurrencyPipe, DecimalPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { ApiImageUrlPipe } from '@pipes/api-image-url.pipe';
+import { PackagesService } from '@services/packages.service';
+import { IPicnicPackage } from '@shared/interfaces';
+import { map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-picnic-combos',
-  imports: [MatIconModule],
+  imports: [AsyncPipe, MatIconModule, DecimalPipe, CurrencyPipe, ApiImageUrlPipe],
   templateUrl: './picnic-combos.component.html',
   styleUrl: './picnic-combos.component.scss'
 })
-export class PicnicCombosComponent {
+export class PicnicCombosComponent implements OnInit {
+  protected readonly packageService = inject(PackagesService);
 
+  public packagesList$: Observable<IPicnicPackage[]> = this.packageService.getPackages()
+    .pipe(map(list => list.sort((a, b) => (a.minPrice || 0) - (b.minPrice || 0))));
+
+  ngOnInit(): void { }
 }
